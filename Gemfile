d3e94292e798330b7192d967e537e7ffbdfdbdd1
Mohit <mohit.kukreja@innostax.com>
2025-03-25 12:16:40 +0530

    Modify linting errors

--- Gemfile
+++ Gemfile
@@ -47,4 +47,4 @@ group :development, :test do
 end
 
 gem "dotenv-rails", "~> 3.1"
-gem 'rack-cors', :require => 'rack/cors'
+gem "rack-cors", require: "rack/cors"
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,7 +16,7 @@
 # end
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '*'  # Change this to your frontend domain in production
-      resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options]
+      origins "*"  # Change this to your frontend domain in production
+      resource "*", headers: :any, methods: [ :get, :post, :patch, :put, :delete, :options ]
     end
   end
